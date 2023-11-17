e616075c12dd55153dd2f9ef7f409aca751e3183
Igor Oganesian <igor.oganesian@gmail.com>
2023-11-17 18:49:54 -0500

    mock notes & map

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "test note 1",
+      content: "note 1 content",
+    },
+    {
+      id: 2,
+      title: "test note 2",
+      content: "note 2 content",
+    },
+    {
+      id: 3,
+      title: "test note 3",
+      content: "note 3 content",
+    },
+  ]);
+
   return (
     <div className='app-container'>
       <form className='note-form'>
@@ -9,13 +28,15 @@ const App = () => {
         <button type='submit'>Add Note</button>
       </form>
       <div className='notes-grid'>
-        <div className='note-item'>
-          <div className='notes-header'>
-            <button>x</button>
+        {notes.map((note) => (
+          <div className='note-item'>
+            <div className='notes-header'>
+              <button>x</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
           </div>
-          <h2>Note Title</h2>
-          <p>Note Content</p>
-        </div>
+        ))}
       </div>
     </div>
   );
